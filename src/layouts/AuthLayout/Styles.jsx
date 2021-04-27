b80c23c8839a9bd9b94db30ef242d1dc19393bcc
Ajmal KV <reallyaju@gmail.com>
2021-04-27 22:58:04 +0530

    styles for login and home

---
+++ src/layouts/AuthLayout/Styles.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const AuthCont = styled.div`
+display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #dc0714;
+  min-height: 100vh;
+`;
+
+export const Wrapper = styled.div`
+min-width: 320px;
+    text-align: center;
+    padding: 32px;
+    border-radius: 16px;
+    background: white;
+`;
+
+export const AuthBrand = styled.div`
+margin-bottom: 24px;
+`;
+
+export const AuthImg = styled.img`
+    max-width: 120px;
+`;
